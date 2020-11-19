e28d3e4b7e076099a80918dea75cdeeca261cb98
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2020-11-19 18:24:51 +0900

    1. clock.js

---
+++ clock.js
@@ -0,0 +1,18 @@
+const clockContainer = document.querySelector(".js-clock");
+const clock = clockContainer.querySelector(".clock");
+
+function getTime(){
+    const date = new Date();
+    const hours = date.getHours(),
+        miniutes = date.getMinutes(),
+        seconds = date.getSeconds();
+
+    clock.innerText = 
+    `${hours < 10 ? `0${hours}` : hours }:${miniutes < 10 ? `0${miniutes}` : miniutes }:${seconds < 10 ? `0${seconds}` : seconds }`;
+}
+
+function init() {
+    setInterval(getTime,1000);
+}
+
+init();
