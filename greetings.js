8fd0e29e4318ddc4257c1204fdf03d81e5c09bde
rapyon9 <64957267+rapyon9@users.noreply.github.com>
2020-11-19 20:32:46 +0900

    2. greeting.js - part 2
    complete.

--- greetings.js
+++ greetings.js
@@ -3,20 +3,38 @@ const greetingsInput = greetingsForm.querySelector("input");
 const greetings = document.querySelector("h3");
 
 const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
 
 function handleSubmit(event){
     event.preventDefault();
     const currentValue = greetingsInput.value;
     greetingsInput.value = "";
     paintGreetings(currentValue);
+    saveName(currentValue);
 }
 
 function askForName(){
+    greetingsForm.classList.add(SHOWING_CN);
     greetingsForm.addEventListener('submit', handleSubmit);
 }
 
 function paintGreetings(text){
-    greetings.innerText = `hello ${text}`;
+    greetingsForm.classList.remove(SHOWING_CN);
+
+    const date = new Date();
+    const hours = date.getHours();
+
+    if(hours >= 5 && hours < 12){
+        greetings.innerText = `Good morning ${text}`;
+    } else if(hours >=12 && hours < 18) {
+        greetings.innerText = `Good afternoon ${text}`;
+    } else {
+        greetings.innerText = `Good evening ${text}`;
+    }
 }
 
 function loadName(){
